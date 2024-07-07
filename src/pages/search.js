486ed48c26afc35849ef0f57b9996559c62464f0
Miller-Liu <millerliu@berkeley.edu>
2024-07-07 02:15:13 -0700

    cleaned up initial code

---
+++ src/pages/search.js
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet";
+
+export default function SearchPage() {
+    return (
+        <>
+            <Helmet>
+                <title>Searching for...</title>
+            </Helmet>
+            <div>
+                <p>testingg</p>
+            </div>
+        </>
+    );
+}
