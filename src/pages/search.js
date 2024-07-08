c55ca012220d52162c53661cd9f98b02e77e9f3a
Miller-Liu <millerliu@berkeley.edu>
2024-07-08 10:57:21 -0700

    search images

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,27 +1,29 @@
+import './search.css'
+
 import { Helmet, HelmetProvider } from "react-helmet-async";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import secret from "../secret";
 
 export default function SearchPage() {
+    // Allows us to redirect to another path
     const navigate = useNavigate()
 
+    // Search up movie by calling TMDB API using search parameters
     const { searchParams } = useParams();
-    const [searchTitle, setSearchTitle] = useState(searchParams || "");
-
-    const searchResult = async() => {
-        if (searchTitle != "") {
+    const [searchResult, setSearchResult] = useState([]);
+    const searchMovies = async() => {
+        if (searchParams) {
             try {
-                console.log(`${secret["Rapid API"]["API_key"]}`);
-                const url = `https://moviedatabase8.p.rapidapi.com/Search/${searchTitle}`;
+                const url = `https://api.themoviedb.org/3/search/multi?query=${searchParams}&include_adult=${false}&language=en-US&page=${1}'`;
                 const options = {
                     method: 'GET',
                     headers: {
-                        'x-rapidapi-key': `${secret["Rapid API"]["API_key"]}`,
-                        'x-rapidapi-host': 'moviedatabase8.p.rapidapi.com'
+                      accept: 'application/json',
+                      Authorization: `Bearer ${secret.TMDB.API_read_access_token}`
                     }
-                };
+                  };
     
                 const response = await fetch(url, options);
                 
@@ -30,12 +32,25 @@ export default function SearchPage() {
                 }
         
                 const result = await response.json();
-                console.log(result);
+                const processedResult = [];
+                
+                if (result.total_results > 0) {
+                    for (const item of result.results) {
+                        processedResult.push({"title": item.name || item.original_title, "cover": `https://image.tmdb.org/t/p/original${item.poster_path}`})
+                    }
+                }
+                setSearchResult([...searchResult, ...processedResult]);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         }
     };
+    useEffect(() => {searchMovies()}, [searchParams]);
+
+    useEffect(() => {console.log(searchResult)}, [searchResult]);
+
+    // Tracks what's entered in the search bar
+    const [searchTitle, setSearchTitle] = useState(searchParams || "");
 
     return (
         <HelmetProvider>
@@ -44,10 +59,17 @@ export default function SearchPage() {
             </Helmet>
             <div>
                 <input type="text" placeholder="Please Enter Title" value={searchTitle} onChange={(e) => setSearchTitle(e.target.value)} />
-                <button onClick={() => navigate(`/search/${searchTitle}`)}>I dont do anything</button>
-                <br/>
-                <br/>
-                <button onClick={() => searchResult()}>Get results</button>
+                <button onClick={() => navigate(`/search/${searchTitle}`)}>Search</button>
+            </div>
+            <div id="ResultsGallery">
+                {searchResult.map((item, index) => {
+                    return (
+                        <div className="MovieBlock" key={`Movie${index + 1}`}>
+                            <img className="MovieCover" src={item.cover}></img>
+                            <div className="MovieTitleContainer">{item.title}</div>
+                        </div>
+                    )
+                })}
             </div>
         </HelmetProvider>
     );
