caa6dd70b9ab5850fa77b86151ba86ca4ae849ec
Miller-Liu <millerliu@berkeley.edu>
2024-08-18 01:16:39 -0700

    played around with backend

--- Backend (Flask)/server.py
+++ Backend (Flask)/server.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def default():
     return {}
+
+@app.route("/test")
+def test():
+    return {"sample result fetched": "test"}
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
--- Frontend (React)/src/pages/Profile/Profile.js
+++ Frontend (React)/src/pages/Profile/Profile.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Profile.css';
 import { Helmet, HelmetProvider } from "react-helmet-async";
 
 export default function Profile() {
+
+  useEffect(() => {
+    fetch("/test").then(
+      res => res.json()
+    ).then(
+      data => console.log(data)
+    );
+  }, [])
+
   return (
     <HelmetProvider>
       <Helmet>
